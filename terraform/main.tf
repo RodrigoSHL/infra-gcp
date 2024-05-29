abb299ddffe00f148651961e8e790bf90600ec75
Rodrigo Catalán <rodrigocatalan@Rodrigos-MacBook-Pro-2.local>
2024-05-29 12:18:48 -0400

    FEAT: update cluster information
    Signed-off-by: Rodrigo Catalán <rodrigocatalan@Rodrigos-MacBook-Pro-2.local>

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,17 +2,19 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
+
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
-
-  # Initial node count
-  initial_node_count = 1
-
-  # Node configuration
-  node_config {
-    machine_type = "e2-small"  # Tipo de instancia más pequeño
-    disk_size_gb = 10          # Tamaño de disco más pequeño
-  }
 }
 
+resource "google_container_node_pool" "primary_nodes" {
+  cluster    = google_container_cluster.primary.name
+  location   = var.region
+  node_count = 1
+
+  node_config {
+    machine_type = "e2-small"
+    disk_size_gb = 10
+  }
+}
