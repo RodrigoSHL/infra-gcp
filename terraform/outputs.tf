690278cfbcd791814a5b74c206a006a6e4347645
Rodrigo Catalán <rodrigocatalan@Rodrigos-MacBook-Pro-2.local>
2024-05-28 20:41:33 -0400

    CHORE: changes in output.tf
    Signed-off-by: Rodrigo Catalán <rodrigocatalan@Rodrigos-MacBook-Pro-2.local>

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,9 @@
 output "gke_cluster_name" {
-  value = module.gke.cluster_name
+  description = "Nombre del clúster de Kubernetes (GKE)"
+  value       = google_container_cluster.primary.name
 }
 
 output "vpn_gateway_ip" {
-  value = module.vpn.vpn_gateway_ip
+  description = "IP de la VPN Gateway"
+  value       = google_compute_vpn_gateway.vpn_gateway.address
 }
