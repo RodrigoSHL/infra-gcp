7a5e1c146351ee5720203efb4bf41280250dcb73
Rodrigo Catalán <rodrigocatalan@Rodrigos-MacBook-Pro-2.local>
2024-05-28 20:59:42 -0400

    CHORE: change cluster resources
    Signed-off-by: Rodrigo Catalán <rodrigocatalan@Rodrigos-MacBook-Pro-2.local>

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,17 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
-
-# Configuración del clúster de Kubernetes (GKE)
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
+
+  # Número inicial de nodos
+  initial_node_count = 1
+
+  # Configuración del nodo
+  node_config {
+    machine_type = "e2-small"  # Tipo de instancia más pequeño
+    disk_size_gb = 10          # Tamaño de disco más pequeño
+  }
 }
+
