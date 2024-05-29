d8a6a912a9b1201da8d32101addb86809f6e8dd4
Rodrigo Catalán <rodrigocatalan@Rodrigos-MacBook-Pro-2.local>
2024-05-29 08:57:06 -0400

    FEAT: add changes in cluster config
    Signed-off-by: Rodrigo Catalán <rodrigocatalan@Rodrigos-MacBook-Pro-2.local>

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,10 +6,10 @@ resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
 
-  # Número inicial de nodos
+  # Initial node count
   initial_node_count = 1
 
-  # Configuración del nodo
+  # Node configuration
   node_config {
     machine_type = "e2-small"  # Tipo de instancia más pequeño
     disk_size_gb = 10          # Tamaño de disco más pequeño
