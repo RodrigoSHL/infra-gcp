107e805641d3d6ad3b768a3f2dcec0b6d637a36a
Rodrigo Catalán <rodrigocatalan@Rodrigos-MacBook-Pro-2.local>
2024-05-29 13:29:28 -0400

    FEAT: update cluster information
    Signed-off-by: Rodrigo Catalán <rodrigocatalan@Rodrigos-MacBook-Pro-2.local>

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,24 +2,16 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
-
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
 
-  # Initial node count is set to 1 to satisfy the requirement
+  # Initial node count
   initial_node_count = 1
 
-  remove_default_node_pool = true  # Remove the default node pool created by GKE
-}
-
-resource "google_container_node_pool" "primary_nodes" {
-  cluster    = google_container_cluster.primary.name
-  location   = var.region
-  node_count = 1  # Define the exact number of nodes here
-
+  # Node configuration
   node_config {
-    machine_type = "e2-small"
-    disk_size_gb = 10
+    machine_type = "e2-small"  # Tipo de instancia más pequeño
+    disk_size_gb = 10          # Tamaño de disco más pequeño
   }
 }
